b0648fd46aedd4ad633ea9105dfaa7cf933c3ca6
Prafull <official.sonawaneprafull@gmail.com>
2023-12-02 14:29:51 +0530

    random joke api added

---
+++ random_joke_api/api.py
@@ -0,0 +1,31 @@
+from random import randint
+from flask import Flask,jsonify
+
+
+def get_joke():
+    with open('jokes.txt','r') as jokes_txt:
+        jokes = [joke.strip() for joke in jokes_txt.readlines()]
+    return jokes
+
+jokes = get_joke()
+
+app = Flask(__name__)
+@app.route("/",methods=['get'])
+def home():
+    path = ['/one-liner','/get-context-joke','/get-joke-list']
+    return jsonify(path)
+
+@app.route("/one-liner",methods=['get'])
+def one_liner() :
+    return jokes[randint(0,len(jokes)-1)]
+
+@app.route("/get-context-joke")
+def get_context_joke():
+    pass
+
+@app.route("/get-joke-list")
+def joke_list():
+    return jsonify(jokes)
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
